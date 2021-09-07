cd6d83921782bb80c89ab203dee320fc64a3f945
Evan-yy <jjjklj@qq.com>
2021-09-07 23:59:18 +0800

    1

---
+++ classes/artifacts/fangwuzushou_Web_exploded/sql/fangwuzushou.sql
@@ -0,0 +1,161 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.0.22-community-nt : Database - fangwuzushou
+*********************************************************************
+*/
+
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`fangwuzushou` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `fangwuzushou`;
+
+/*Table structure for table `bbs` */
+
+DROP TABLE IF EXISTS `bbs`;
+
+CREATE TABLE `bbs` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(255) default NULL,
+  `pubtime` varchar(30) default NULL,
+  `uid` int(11) default NULL,
+  `bid` int(11) default NULL,
+  `sta` varchar(10) default NULL,
+  `note` text,
+  `note2` text,
+  `htime` varchar(30) default NULL,
+  `btype` varchar(10) default NULL,
+  `hid` int(11) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `bbs` */
+
+insert  into `bbs`(`id`,`name`,`pubtime`,`uid`,`bid`,`sta`,`note`,`note2`,`htime`,`btype`,`hid`) values (2,'我的留言11','2020-04-01 21:59:51',5,NULL,'待回复','这里是内容看看看看！<img src=\"http://localhost:8080/fangwuzushou/kindeditor/plugins/emoticons/images/13.gif\" border=\"0\" alt=\"\" />',NULL,NULL,'留言',NULL),(3,'留言标题','2020-04-01 21:59:51',6,NULL,'待回复','这里是内容',NULL,NULL,'留言',NULL),(4,'留言11','2020-04-01 21:59:51',7,NULL,'待回复','333',NULL,NULL,'留言',NULL),(6,'留言88','2020-04-03 22:01:27',9,NULL,'待回复','内容3333',NULL,NULL,'留言',NULL),(7,NULL,NULL,NULL,6,'回复',NULL,'评论2222','2020-04-03 22:01:38','回复',9),(8,NULL,NULL,NULL,2,'回复',NULL,'评论4444','2020-04-03 22:01:48','回复',9);
+
+/*Table structure for table `ftype` */
+
+DROP TABLE IF EXISTS `ftype`;
+
+CREATE TABLE `ftype` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(255) default NULL,
+  `btype` varchar(20) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `ftype` */
+
+insert  into `ftype`(`id`,`name`,`btype`) values (1,'三室一厅一厨一卫',NULL),(2,'二室一厅一卫',NULL),(3,'分类03',NULL),(4,'三室一厅',NULL),(5,'一室一厅一卫',NULL),(6,'四室一厅一厨一卫',NULL),(7,'单间',NULL);
+
+/*Table structure for table `goods` */
+
+DROP TABLE IF EXISTS `goods`;
+
+CREATE TABLE `goods` (
+  `id` int(11) NOT NULL auto_increment,
+  `fid` int(11) default NULL,
+  `img` varchar(255) default NULL,
+  `name` varchar(255) default NULL,
+  `price` double default NULL,
+  `note` text,
+  `status` varchar(50) default NULL,
+  `pubtime` varchar(100) default NULL,
+  `iscommend` varchar(20) default NULL,
+  `uid` varchar(20) default NULL,
+  `btype` varchar(20) default NULL,
+  `area` double default NULL,
+  `addr` varchar(255) default NULL,
+  `tel` varchar(30) default NULL,
+  `tname` varchar(255) default NULL,
+  `zprice` double default NULL,
+  `price1` double default NULL,
+  PRIMARY KEY  (`id`),
+  KEY `FK_flower` (`fid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `goods` */
+
+insert  into `goods`(`id`,`fid`,`img`,`name`,`price`,`note`,`status`,`pubtime`,`iscommend`,`uid`,`btype`,`area`,`addr`,`tel`,`tname`,`zprice`,`price1`) values (1,1,'1.jpg','房屋1',5000,'<img src=\"/fangwuzushou/attached/image/20190413/20190413184631_539.jpg\" width=\"600\" height=\"263\" alt=\"\" />','通过审核','2020-04-01 21:59:13',NULL,'1','出租',102,'位置1','15396232144','联系人1',NULL,3500),(2,1,'3.jpg','出租房屋1',3600,'<img src=\"/fangwuzushou/attached/image/20190413/20190413184849_71.jpg\" alt=\"\" />','通过审核','2020-04-01 21:59:13',NULL,'1','出租',100,'位置2','13652348756','联系人2',NULL,2850),(5,1,'6.jpg','出租房屋3',4400,'<p>\r\n	房屋图片说明：\r\n</p>\r\n<p>\r\n	<img src=\"/fangwuzushou/attached/image/20190413/20190413190947_372.jpg\" alt=\"\" /> \r\n</p>','通过审核','2020-04-01 21:59:13',NULL,'1','出租',99,'位置1','13652348756','刘三',NULL,750),(7,1,'5.jpg','c出租01',3200,'房屋说明！！<img src=\"/fangwuzushou/attached/image/20190421/20190421111248_901.jpg\" alt=\"\" />','通过审核','2020-04-01 21:59:13','非推荐','6','出租',88,'位置1','15396232144','测试用户02',NULL,3200),(9,1,'lv02.jpg','房屋11',3600,'房屋合同<a class=\"ke-insertfile\" href=\"/fangwuzushou/attached/file/20200223/20200223173511_104.docx\" target=\"_blank\">/fangwuzushou/attached/file/20200223/20200223173511_104.docx</a>','通过审核','2020-04-01 21:59:13',NULL,'1','出租',88,'位置1','14584845215','测试用户',NULL,3500),(10,1,'4.jpg','出租88',3600,'房屋说明如下！','通过审核','2020-04-01 21:59:13','非推荐','7','出租',100,'位置1','15584845215','测试用户',NULL,2900),(12,1,'20171014083653557.jpg','出售99',10001,'说明如下','通过审核','2020-04-01 21:59:13','非推荐','7','出售',96,'位置1','15584845215','测试用户',960000,NULL),(13,5,'lv02.jpg','单间出租',650,'出租说明111','通过审核','2020-04-03 21:45:25','非推荐','9','出租',15,'地址1','13556212314','测试用户04',NULL,650),(14,4,'8.jpg','三室一厅2002',8500,'说明如下','通过审核','2020-04-03 21:48:39','非推荐','9','出售',95,'地址2','13556212314','测试用户04',807500,NULL);
+
+/*Table structure for table `keep` */
+
+DROP TABLE IF EXISTS `keep`;
+
+CREATE TABLE `keep` (
+  `id` int(11) NOT NULL auto_increment,
+  `uid` int(11) default NULL,
+  `fid` int(11) default NULL,
+  `status` varchar(30) default NULL,
+  `isdel` varchar(50) default NULL,
+  `pubtime` varchar(50) default NULL,
+  PRIMARY KEY  (`id`),
+  KEY `FK_keep` (`fid`),
+  CONSTRAINT `FK_keep` FOREIGN KEY (`fid`) REFERENCES `goods` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `keep` */
+
+insert  into `keep`(`id`,`uid`,`fid`,`status`,`isdel`,`pubtime`) values (1,6,NULL,'收藏','1','2020-04-01 21:59:27'),(2,5,NULL,'收藏','1','2020-04-01 21:59:27'),(3,7,1,'收藏','1','2020-04-01 21:59:27'),(4,9,12,'收藏','1','2020-04-03 22:02:06'),(5,9,12,'购买','1','2020-04-03 22:02:10');
+
+/*Table structure for table `news` */
+
+DROP TABLE IF EXISTS `news`;
+
+CREATE TABLE `news` (
+  `ggid` int(11) NOT NULL auto_increment,
+  `ggpic` varchar(255) default NULL,
+  `name` varchar(255) default NULL,
+  `note` text,
+  `isdel` varchar(20) default NULL,
+  `gtype` varchar(50) default NULL,
+  `pubtime` varchar(30) default NULL,
+  PRIMARY KEY  (`ggid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `news` */
+
+insert  into `news`(`ggid`,`ggpic`,`name`,`note`,`isdel`,`gtype`,`pubtime`) values (1,'20201014082538766.jpg','新闻22','<img src=\"/fangwuzushou/attached/image/20190316/20190316151430_717.jpg\" alt=\"\" />说明','1','新闻动态','2020-04-01 21:59:34'),(2,'20201014083549392.jpg','新闻2','<img src=\"/fangwuzushou/attached/image/20190316/20190316151455_347.jpg\" alt=\"\" />','1','新闻动态','2020-04-01 21:59:34'),(3,'20201014083549392.jpg','新闻3','内容','1','新闻动态','2020-04-01 21:59:34'),(4,'20201014083717838.jpg','房屋新闻11','<img src=\"/fangwuzushou/attached/image/20190413/20190413183906_985.jpg\" width=\"600\" height=\"349\" alt=\"\" />','1','新闻动态','2020-04-01 21:59:34'),(5,'12.jpg','房屋动态777','<p>\r\n	这个是内容\r\n</p>\r\n<p>\r\n	<img src=\"/fangwuzushou/attached/image/20190413/20190413190748_543.jpg\" width=\"600\" height=\"241\" alt=\"\" />\r\n</p>','1','新闻动态','2020-04-01 21:59:34'),(6,'20201014083717838.jpg','房屋动态88','说明','1','新闻动态','2020-04-01 21:59:34'),(7,'2.jpg','租赁合同11','<a class=\"ke-insertfile\" href=\"/fangwuzushou/attached/file/20200226/20200226173936_887.docx\" target=\"_blank\">租赁合同</a>','1','新闻动态','2020-04-01 21:59:34'),(8,'5.jpg','租赁合同88','<a class=\"ke-insertfile\" href=\"/fangwuzushou/attached/file/20200226/20200226174032_482.docx\" target=\"_blank\">租赁合同88</a>','1','新闻动态','2020-04-01 21:59:34'),(9,'20181103023303565.jpg','新闻999','<p>\r\n	新闻的内容\r\n</p>\r\n<p>\r\n	<img src=\"/fangwuzushou/attached/image/20200403/20200403220252_563.jpg\" width=\"800\" height=\"351\" alt=\"\" />\r\n</p>','1','新闻动态','2020-04-03 22:03:03');
+
+/*Table structure for table `sysuser` */
+
+DROP TABLE IF EXISTS `sysuser`;
+
+CREATE TABLE `sysuser` (
+  `uid` int(11) NOT NULL auto_increment,
+  `uname` varchar(20) default NULL,
+  `sex` varchar(10) default NULL,
+  `address` varchar(100) default NULL,
+  `pwd` varchar(20) default NULL,
+  `utype` varchar(30) default NULL,
+  `tel` varchar(20) default NULL,
+  `age` varchar(10) default NULL,
+  `mbanswer` varchar(30) default NULL,
+  `question` varchar(30) default NULL,
+  `email` varchar(30) default NULL,
+  `pubtime` varchar(30) default NULL,
+  `tname` varchar(50) default NULL,
+  `phone` varchar(30) default NULL,
+  `qq` varchar(255) default NULL,
+  `weixin` varchar(255) default NULL,
+  `isdel` varchar(10) default NULL,
+  `ctype` varchar(20) default NULL,
+  `img` varchar(255) default NULL,
+  PRIMARY KEY  (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `sysuser` */
+
+insert  into `sysuser`(`uid`,`uname`,`sex`,`address`,`pwd`,`utype`,`tel`,`age`,`mbanswer`,`question`,`email`,`pubtime`,`tname`,`phone`,`qq`,`weixin`,`isdel`,`ctype`,`img`) values (1,'admin','男','武汉343号','123','管理员','14584845215',NULL,'我是谁？','admin','21521515@qq.com','2020-04-01 21:59:44',NULL,NULL,NULL,'weixin1',NULL,NULL,NULL),(2,'hy01','男','武汉343号','123456','会员','14584845215',NULL,'我是谁？','hy01','21521515@qq.com','2020-04-01 21:59:44','会员01',NULL,NULL,'weixin1','1',NULL,'20200915112433524.jpg'),(5,'test01','女','地址1','123456','会员','15396232145',NULL,'我是谁？','test01','61515115@qq.com','2020-04-01 21:59:44','测试用户01',NULL,NULL,'weixin3','1',NULL,'20170915112433524.jpg'),(6,'test02','女','地址1','123456','会员','15396232145',NULL,'我是谁？','test02','61515115@qq.com','2020-04-01 21:59:44','测试用户02',NULL,NULL,'weixin3','1',NULL,'20170915113845579.jpg'),(7,'test','男','地区1','123456','会员','15584845215',NULL,'我是谁？','test','21521515@qq.com','2020-04-01 21:59:44','测试用户',NULL,NULL,'weixin1','1',NULL,'20170915112724145.jpg'),(8,'yh88','男','地区1','123456','会员','14584845288',NULL,'我是谁？','yh88',NULL,'2020-04-01 21:59:44','用户88',NULL,NULL,NULL,'1',NULL,'20170915113845579.jpg'),(9,'test04','女','地址2','123456','会员','13556212314',NULL,'我是谁？','test04','115431678@qq.com','2020-04-03 21:44:15','测试用户04',NULL,NULL,'weixin2','1',NULL,'20170910095708668.jpg');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
---
+++ src/com/entity/Goods.java
@@ -0,0 +1,173 @@
+package com.entity;
+
+public class Goods {
+    private Integer id;
+
+    private Integer fid;
+
+    private String img;
+
+    private String name;
+
+    private Double price;
+
+    private String status;
+
+    private String pubtime;
+
+    private String iscommend;
+
+    private String uid;
+
+    private String btype;
+
+    private Double area;
+
+    private String addr;
+
+    private String tel;
+
+    private String tname;
+
+    private Double zprice;
+
+    private Double price1;
+
+    private String note;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getFid() {
+        return fid;
+    }
+
+    public void setFid(Integer fid) {
+        this.fid = fid;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img == null ? null : img.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+
+    public String getPubtime() {
+        return pubtime;
+    }
+
+    public void setPubtime(String pubtime) {
+        this.pubtime = pubtime == null ? null : pubtime.trim();
+    }
+
+    public String getIscommend() {
+        return iscommend;
+    }
+
+    public void setIscommend(String iscommend) {
+        this.iscommend = iscommend == null ? null : iscommend.trim();
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid == null ? null : uid.trim();
+    }
+
+    public String getBtype() {
+        return btype;
+    }
+
+    public void setBtype(String btype) {
+        this.btype = btype == null ? null : btype.trim();
+    }
+
+    public Double getArea() {
+        return area;
+    }
+
+    public void setArea(Double area) {
+        this.area = area;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr == null ? null : addr.trim();
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel == null ? null : tel.trim();
+    }
+
+    public String getTname() {
+        return tname;
+    }
+
+    public void setTname(String tname) {
+        this.tname = tname == null ? null : tname.trim();
+    }
+
+    public Double getZprice() {
+        return zprice;
+    }
+
+    public void setZprice(Double zprice) {
+        this.zprice = zprice;
+    }
+
+    public Double getPrice1() {
+        return price1;
+    }
+
+    public void setPrice1(Double price1) {
+        this.price1 = price1;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note == null ? null : note.trim();
+    }
+}
